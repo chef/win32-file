b8cfdb5219e50d5ff1debeb50542375c876f4d3b
Daniel Berger <djberg96@gmail.com>
2012-03-25 10:59:28 -0600

    Updated version test and fixed the blockdev test.

--- test/test_win32_file_attributes.rb
+++ test/test_win32_file_attributes.rb
@@ -26,21 +26,21 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
    end
 
    def test_version
-      assert_equal('0.6.7', File::WIN32_FILE_VERSION)
+      assert_equal('0.6.8', File::WIN32_FILE_VERSION)
    end
-   
+
    def test_is_directory
       assert_true(File.directory?(Dir.pwd))
       assert_false(File.directory?(@@file))
-      assert_false(File.directory?('C:/aaabbbccc'))      
+      assert_false(File.directory?('C:/aaabbbccc'))
    end
-   
+
    def test_temporary
       assert_respond_to(File, :temporary?)
-      assert_nothing_raised{ File.temporary?(@@file) }    
+      assert_nothing_raised{ File.temporary?(@@file) }
       assert_equal(false, File.temporary?(@@file))
    end
-   
+
    def test_temporary_instance
       assert_respond_to(@fh, :temporary=)
       assert_nothing_raised{ @fh.temporary = true }
@@ -51,13 +51,13 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.temporary? }
       assert_raises(ArgumentError){ File.temporary?(@@file, 'foo') }
    end
-   
+
    def test_system
       assert_respond_to(File, :system?)
-      assert_nothing_raised{ File.system?(@@file) }    
+      assert_nothing_raised{ File.system?(@@file) }
       assert_equal(false, File.system?(@@file))
    end
-   
+
    def test_system_instance
       assert_respond_to(@fh, :system=)
       assert_nothing_raised{ @fh.system = true }
@@ -68,13 +68,13 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.system? }
       assert_raises(ArgumentError){ File.system?(@@file, 'foo') }
    end
-   
+
    def test_sparse
       assert_respond_to(File, :sparse?)
-      assert_nothing_raised{ File.sparse?(@@file) }    
+      assert_nothing_raised{ File.sparse?(@@file) }
       assert_equal(false, File.sparse?(@@file))
    end
-   
+
    # I don't actually test assignment here since making a file a sparse
    # file can't be undone.
    def test_sparse_instance
@@ -85,10 +85,10 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.sparse? }
       assert_raises(ArgumentError){ File.sparse?(@@file, 'foo') }
    end
-   
+
    def test_reparse_point
       assert_respond_to(File, :reparse_point?)
-      assert_nothing_raised{ File.reparse_point?(@@file) }    
+      assert_nothing_raised{ File.reparse_point?(@@file) }
       assert_equal(false, File.reparse_point?(@@file))
    end
 
@@ -96,10 +96,10 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.reparse_point? }
       assert_raises(ArgumentError){ File.reparse_point?(@@file, 'foo') }
    end
-   
+
    def test_readonly
       assert_respond_to(File, :readonly?)
-      assert_nothing_raised{ File.readonly?(@@file) }    
+      assert_nothing_raised{ File.readonly?(@@file) }
       assert_equal(false, File.readonly?(@@file))
    end
 
@@ -107,7 +107,7 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_respond_to(File, :read_only?)
       assert_equal(true, File.method(:readonly?) == File.method(:read_only?))
    end
-   
+
    def test_readonly_instance
       assert_respond_to(@fh, :readonly=)
       assert_nothing_raised{ @fh.readonly = true }
@@ -118,13 +118,13 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.read_only? }
       assert_raises(ArgumentError){ File.read_only?(@@file, 'foo') }
    end
-   
+
    def test_offline
       assert_respond_to(File, :offline?)
-      assert_nothing_raised{ File.offline?(@@file) }    
+      assert_nothing_raised{ File.offline?(@@file) }
       assert_equal(false, File.offline?(@@file))
    end
-   
+
    def test_offline_instance
       assert_respond_to(@fh, :offline=)
       assert_nothing_raised{ @fh.offline =  true }
@@ -135,13 +135,13 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.offline? }
       assert_raises(ArgumentError){ File.offline?(@@file, 'foo') }
    end
-   
+
    def test_normal
       assert_respond_to(File, :normal?)
-      assert_nothing_raised{ File.normal?(@@file) }    
+      assert_nothing_raised{ File.normal?(@@file) }
       assert_equal(false, File.normal?(@@file))
    end
-   
+
    def test_normal_instance
       assert_respond_to(@fh, :normal=)
       assert_nothing_raised{ @fh.normal = true }
@@ -153,13 +153,13 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_raises(ArgumentError){ File.normal?(@@file, 'foo') }
       assert_raises(ArgumentError){ @fh.normal = false }
    end
-   
+
    def test_hidden
       assert_respond_to(File, :hidden?)
-      assert_nothing_raised{ File.hidden?(@@file) }    
+      assert_nothing_raised{ File.hidden?(@@file) }
       assert_equal(false, File.hidden?(@@file))
    end
-   
+
    def test_hidden_instance
       assert_respond_to(@fh, :hidden=)
       assert_nothing_raised{ @fh.hidden = true }
@@ -169,22 +169,22 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
    def test_hidden_expected_errors
       assert_raises(ArgumentError){ File.hidden? }
       assert_raises(ArgumentError){ File.hidden?(@@file, 'foo') }
-   end 
+   end
 
    def test_encrypted
       assert_respond_to(File, :encrypted?)
-      assert_nothing_raised{ File.encrypted?(@@file) }    
+      assert_nothing_raised{ File.encrypted?(@@file) }
       assert_equal(false, File.encrypted?(@@file))
    end
 
    def test_encrypted_expected_errors
       assert_raises(ArgumentError){ File.encrypted? }
       assert_raises(ArgumentError){ File.encrypted?(@@file, 'foo') }
-   end    
+   end
 
    def test_indexed
       assert_respond_to(File, :indexed?)
-      assert_nothing_raised{ File.indexed?(@@file) }    
+      assert_nothing_raised{ File.indexed?(@@file) }
       assert_equal(true, File.indexed?(@@file))
    end
 
@@ -192,7 +192,7 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_respond_to(File, :content_indexed?)
       assert(File.method(:content_indexed?) == File.method(:indexed?))
    end
-   
+
    def test_indexed_instance
       assert_respond_to(@fh, :indexed=)
       assert_nothing_raised{ @fh.indexed = true }
@@ -202,14 +202,14 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
    def test_indexed_expected_errors
       assert_raises(ArgumentError){ File.indexed? }
       assert_raises(ArgumentError){ File.indexed?(@@file, 'foo') }
-   end    
+   end
 
    def test_compressed
       assert_respond_to(File, :compressed?)
-      assert_nothing_raised{ File.compressed?(@@file) }    
+      assert_nothing_raised{ File.compressed?(@@file) }
       assert_equal(false, File.compressed?(@@file))
    end
-   
+
    # We have to explicitly reset the compressed attribute to false as
    # the last of these assertions.
    def test_compressed_instance
@@ -226,31 +226,31 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
    def test_compressed_expected_errors
       assert_raises(ArgumentError){ File.compressed? }
       assert_raises(ArgumentError){ File.compressed?(@@file, 'foo') }
-   end    
-   
+   end
+
    def test_archive
       assert_respond_to(File, :archive?)
-      assert_nothing_raised{ File.archive?(@@file) }    
+      assert_nothing_raised{ File.archive?(@@file) }
       assert_equal(true, File.archive?(@@file))
    end
-   
+
    def test_archive_instance
       assert_respond_to(@fh, :archive=)
       assert_nothing_raised{ @fh.archive = false }
       assert_equal(false, File.archive?(@@file))
    end
-   
+
    def test_archive_expected_errors
       assert_raises(ArgumentError){ File.archive? }
       assert_raises(ArgumentError){ File.archive?(@@file, 'foo') }
    end
-   
+
    def test_attributes
       assert_respond_to(File, :attributes)
       assert_kind_of(Array, File.attributes(@@file))
       assert_equal(['archive', 'indexed'], File.attributes(@@file))
    end
-   
+
    def test_set_attributes
       assert_respond_to(File, :set_attributes)
       assert_nothing_raised{ File.set_attributes(@@file, File::HIDDEN) }
@@ -261,12 +261,12 @@ class TC_Win32_File_Attributes < Test::Unit::TestCase
       assert_respond_to(File, :set_attr)
       assert(File.method(:set_attr) == File.method(:set_attributes))
    end
-   
+
    def test_remove_attributes
       assert_respond_to(File, :remove_attributes)
       assert_nothing_raised{ File.remove_attributes(@@file, File::ARCHIVE) }
       assert_equal(false, File.archive?(@@file))
-   end   
+   end
 
    def test_unset_attr_alias
       assert_respond_to(File, :unset_attr)
--- test/test_win32_file_stat.rb
+++ test/test_win32_file_stat.rb
@@ -19,6 +19,8 @@ class TC_Win32_File_Stat < Test::Unit::TestCase
       @@file = File.join(Dir.pwd, 'stat_test.txt')
       File.open(@@file, 'w'){ |fh| fh.puts "This is a test." }
 
+      @@block_dev = nil
+
       # Find a block device
       'A'.upto('Z'){ |volume|
          volume += ":\\"
@@ -35,7 +37,7 @@ class TC_Win32_File_Stat < Test::Unit::TestCase
       assert_respond_to(File, :size)
       assert_equal(17, File.size(@@file))
    end
-   
+
    def test_blksize
       msg = "+The blksize may be different - ignore+"
       assert_respond_to(File, :blksize)
@@ -43,58 +45,59 @@ class TC_Win32_File_Stat < Test::Unit::TestCase
       assert_kind_of(Fixnum, File.blksize(@@file))
       assert_equal(4096, File.blksize(@@file), msg)
    end
-   
+
    def test_blksize_expected_errors
       assert_raises(ArgumentError){ File.blksize }
       assert_raises(ArgumentError){ File.blksize(@@file, "foo") }
    end
-   
+
    # The test for a block device will fail unless the D: drive is a CDROM.
    def test_blockdev
       assert_respond_to(File, :blockdev?)
-      assert_nothing_raised{ File.blockdev?("C:\\") }    
+      assert_nothing_raised{ File.blockdev?("C:\\") }
       assert_equal(false, File.blockdev?("NUL"))
 
+      omit_unless(@@block_dev, "Could not find block device")
       omit_unless(File.exists?(@@block_dev), "No media in device - skipping")
       assert_equal(true, File.blockdev?(@@block_dev))
    end
-   
+
    def test_blockdev_expected_errors
       assert_raises(ArgumentError){ File.blockdev? }
       assert_raises(ArgumentError){ File.blockdev?(@@file, "foo") }
    end
-   
+
    def test_chardev
       assert_respond_to(File, :chardev?)
-      assert_nothing_raised{ File.chardev?("NUL") }     
+      assert_nothing_raised{ File.chardev?("NUL") }
       assert_equal(true, File.chardev?("NUL"))
       assert_equal(false, File.chardev?(@@file))
    end
-   
+
    def test_chardev_expected_errors
       assert_raises(ArgumentError){ File.chardev? }
       assert_raises(ArgumentError){ File.chardev?(@@file, "foo") }
    end
-   
+
    # Ensure that not only does the File class respond to the stat method,
    # but also that it's returning the File::Stat class from the
    # win32-file-stat package.
-   # 
+   #
    def test_stat_class
       assert_respond_to(File, :stat)
       assert_kind_of(File::Stat, File.stat(@@file))
       assert_equal(false, File.stat(@@file).hidden?)
    end
-   
+
    # This was added after it was discovered that lstat is not aliased
    # to stat automatically on Windows.
-   # 
+   #
    def test_lstat_class
       assert_respond_to(File, :lstat)
       assert_kind_of(File::Stat, File.lstat(@@file))
       assert_equal(false, File.stat(@@file).hidden?)
    end
-   
+
    def test_stat_instance
       File.open(@@file){ |f|
          assert_respond_to(f, :stat)
@@ -102,7 +105,7 @@ class TC_Win32_File_Stat < Test::Unit::TestCase
          assert_equal(false, f.stat.hidden?)
       }
    end
-   
+
    def self.shutdown
       File.delete(@@file) if File.exists?(@@file)
       @@file = nil
